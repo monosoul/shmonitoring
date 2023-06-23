eef169b4f9d674f55364ef8cb88f2233500f484f
monosoul <andrei.nevedomskii@wolt.com>
2023-06-23 14:00:50 +0200

    Add find method to repository

--- type-safe-with-jooq/src/main/kotlin/dev/monosoul/shmonitoring/persistence/EventsRepository.kt
+++ type-safe-with-jooq/src/main/kotlin/dev/monosoul/shmonitoring/persistence/EventsRepository.kt
@@ -1,11 +1,14 @@
 package dev.monosoul.shmonitoring.persistence
 
-import dev.monosoul.shmonitoring.generated.tables.Events
 import dev.monosoul.shmonitoring.generated.tables.records.EventsRecord
+import dev.monosoul.shmonitoring.generated.tables.references.EVENTS
 import dev.monosoul.shmonitoring.model.EventId
 import dev.monosoul.shmonitoring.model.ServiceStatus
+import dev.monosoul.shmonitoring.model.ShmonitoringEventFilter
 import dev.monosoul.shmonitoring.model.ShmonitoringEventRequest
+import dev.monosoul.shmonitoring.model.ShmonitoringEventResponse
 import org.jooq.DSLContext
+import org.jooq.impl.DSL
 import java.time.Clock
 import java.time.LocalDateTime
 import java.util.UUID
@@ -16,10 +19,29 @@ class EventsRepository(
     private val generateId: () -> EventId = { EventId(UUID.randomUUID()) },
 ) {
     fun save(event: ShmonitoringEventRequest<ServiceStatus>) {
-        db.insertInto(Events.EVENTS)
+        db.insertInto(EVENTS)
             .set(event.toRecord())
             .execute()
     }
+
+    fun find(filter: ShmonitoringEventFilter): List<ShmonitoringEventResponse<ServiceStatus>> =
+        db.selectFrom(EVENTS)
+            .where(filter.toCondition())
+            .fetch { it.toResponse() }
+
+    private fun ShmonitoringEventFilter.toCondition() = DSL.and(
+        listOfNotNull(
+            hostName?.let(EVENTS.HOST_NAME::eq),
+            serviceName?.let(EVENTS.SERVICE_NAME::eq),
+            owningTeamName?.let(EVENTS.OWNING_TEAM_NAME::eq),
+        )
+    )
+
+    private fun EventsRecord.toResponse() = ShmonitoringEventResponse(
+        base = ShmonitoringEventRequest(timestamp, hostName, serviceName, owningTeamName, serviceStatus),
+        receivedTimestamp = receivedTimestamp,
+        id = id,
+    )
 
     private fun ShmonitoringEventRequest<*>.toRecord() = EventsRecord(
         generateId(),
